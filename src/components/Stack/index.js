14f09782547ca640e1c0884302f25280b692ad65
Davide <davide.ciulla@ticino.com>
2020-04-01 11:52:03 +0200

    debug

--- src/components/Stack/index.js
+++ src/components/Stack/index.js
@@ -1,32 +1,32 @@
 import React, { useState } from "react"
 import Header from "../Header"
-import Object from "../Object"
+// import Object from "../Object"
 
 function Stack() {
 	const {totalObjectsCreated, setTotalObjectsCreated} = useState(0)
 	const {objects, setObjects} = useState([])
 
 	addBlock = () => {
-		const newBlock = <Object 
-							key={newObjectsCount}  
-							id={newObjectsCount}  
-							region="stack"
-							removeBlock={this.removeBlock} 
-						/>
-		setTotalObjectsCreated(oldCount => oldCount+1)
-		setObjects(existingBlocks => [newBlock, ...existingBlocks])
+		// const newBlock = <Object 
+		// 					key={newObjectsCount}  
+		// 					id={newObjectsCount}  
+		// 					region="stack"
+		// 					removeBlock={this.removeBlock} 
+		// 				/>
+		// setTotalObjectsCreated(oldCount => oldCount+1)
+		// setObjects(existingBlocks => [newBlock, ...existingBlocks])
 	}
 
 	removeBlock = (id) => {
-		setObjects(objects => objects.filter(object => id !== object.props.id))
+		// setObjects(objects => objects.filter(object => id !== object.props.id))
 	}
 
 	return (
 		<div className="stack">
 			<Header 
 				region="stack" 
-				numberOfObjects={this.state.objects.length} 
-				addBlock={this.addBlock} 
+				numberOfObjects={objects.length} 
+				addBlock={addBlock} 
 			/>
 			{/* <div className="stack__objects">
 				{this.state.objects}
