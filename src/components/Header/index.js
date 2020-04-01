cf5db7d85408b6e60df6e0d86dca579508e96bb2
Davide <davide.ciulla@ticino.com>
2020-04-01 12:03:53 +0200

    fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import addBlockButton from "../../images/add-block.svg"
 function Header(props) {
 	return (
 		<header>
-			<h1>{props.region.toUpperCase()} ({props.numberOfObjects})</h1>
+			<h1>{props.region.toUpperCase()} (0)</h1>
 			<button>
 				<img src={addBlockButton} alt="Add a block" />
 			</button>
