d2c45fb730d476dc09f2280b0cab3975549cc886
Davide <davide.ciulla@ticino.com>
2020-04-01 11:30:09 +0200

    debug

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Separator from "./../Separator"
 
 class App extends Component {
 
 	render() {
 		return (
 			<div className="App">
+				<Separator />
 				Hello World
 			</div>
 		)
