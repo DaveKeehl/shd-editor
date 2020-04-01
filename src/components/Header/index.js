fb9e36b2eba697416e248fda71b169e9d9288e79
Davide <davide.ciulla@ticino.com>
2020-04-01 12:20:29 +0200

    debug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import addBlockButton from "../../images/add-block.svg"
 function Header(props) {
 	return (
 		<header>
-			<h1>{props.region.toUpperCase()} (0)</h1>
+			<h1>{props.region.toUpperCase()} ({props.objectsCount})</h1>
 			<button>
 				<img src={addBlockButton} alt="Add a block" />
 			</button>
--- src/components/Stack/index.js
+++ src/components/Stack/index.js
@@ -1,27 +1,25 @@
 import React, { useState } from "react"
 import Header from "../Header"
-// import Object from "../Object"
+import Object from "../Object"
 
 function Stack() {
-	// const {totalObjectsCreated, setTotalObjectsCreated} = useState(0)
+	const [totalObjectsCreated, setTotalObjectsCreated] = useState(0)
 	const [objects, setObjects] = useState([])
 
-	// addBlock = () => {
-	// 	const newBlock = <Object 
-	// 						key={newObjectsCount}  
-	// 						id={newObjectsCount}  
-	// 						region="stack"
-	// 						removeBlock={this.removeBlock} 
-	// 					/>
-	// 	setTotalObjectsCreated(oldCount => oldCount+1)
-	// 	setObjects(existingBlocks => [newBlock, ...existingBlocks])
-	// }
+	addBlock = () => {
+		const newBlock = <Object 
+							key={totalObjectsCreated}  
+							id={totalObjectsCreated}  
+							region="stack"
+							removeBlock={removeBlock} 
+						/>
+		setTotalObjectsCreated(oldCount => oldCount+1)
+		setObjects(existingBlocks => [newBlock, ...existingBlocks])
+	}
 
-	// removeBlock = (id) => {
-		// setObjects(objects => objects.filter(object => id !== object.props.id))
-	// }
-
-	console.log(objects)
+	removeBlock = (id) => {
+		setObjects(objects => objects.filter(object => id !== object.props.id))
+	}
 
 	return (
 		<div className="stack">
