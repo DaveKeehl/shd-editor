d44e13002e637a7aa40988674255754b315ac8d1
Davide <davide.ciulla@ticino.com>
2020-04-01 12:33:49 +0200

    debug

--- src/components/Object/index.js
+++ src/components/Object/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-// import ObjectHeader from './ObjectHeader'
+import ObjectHeader from './ObjectHeader'
 // import Variable from './Variable'
 // import NewVariableForm from './NewVariableForm'
 
