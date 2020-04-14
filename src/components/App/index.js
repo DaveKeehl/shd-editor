0bcd06eeafd814ff9f0725f8c805a360584aeb58
Davide <davide.ciulla@ticino.com>
2020-04-14 10:44:50 +0200

    stack is now resizable again

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,46 @@
-import React from "react"
+import React, {useState, useRef} from "react"
 import Stack from "../Stack"
 import Heap from "../Heap"
 import {HeapAddModeContextProvider} from "../../contexts/heapAddModeContext"
 
 function App() {
+	const [isResizable, setIsResizable] = useState(false)
+	const [stackWidth, setStackWidth] = useState(360)
+
+	const separator = useRef(null)
+
+	function handleMouseDown() {
+		setIsResizable(true)
+	}
+
+	function handleMouseUp() {
+		setIsResizable(false)
+	}
+
+	function handleMouseMove(event) {
+		const {clientX} = event
+		if (isResizable && clientX >= 360 && clientX <= 500) {
+			setStackWidth(clientX)
+		}
+	}
+
 	return (
-		<div className="App">
+		<div 
+			className="App"
+			onMouseUp={handleMouseUp}
+			onMouseMove={handleMouseMove}
+			style={{gridTemplateColumns: `${stackWidth}px min-content auto`}}
+		>
 			<Stack />
-			<div className="separator"></div>
+			<div 
+				className="separator" 
+				ref={separator}
+				onMouseDown={handleMouseDown}
+				onMouseUp={handleMouseUp}
+			>
+			</div>
 			<HeapAddModeContextProvider>
-				<Heap />
+				<Heap stackWidth={stackWidth} />
 			</HeapAddModeContextProvider>
 		</div>
 	)
